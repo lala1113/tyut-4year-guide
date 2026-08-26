cd757d2f64734e17fd64fced347468ccd2ea0e89
lala1113 <3132104492@qq.com>
2026-08-26 10:45:34 +0800

    feat: add Excel course credit resource

--- js/changelog.js
+++ js/changelog.js
@@ -5,7 +5,7 @@
 (function () {
   'use strict';
 
-  var CURRENT_VERSION = '3.1.0';
+  var CURRENT_VERSION = '3.1.2';
   var STORAGE_KEY = 'tyutCareerGuide.v3.changelogSeen';
 
   function readSeenVersion() {
